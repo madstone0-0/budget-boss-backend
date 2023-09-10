92588a8143837439dde264e78de650a640b1be91
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-09-10 00:08:54 +0000

    feat(db): Added prepared statements for selection

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -12,11 +12,29 @@ export const users = pgTable("users", {
 export type User = InferSelectModel<typeof users>;
 export type NewUser = InferInsertModel<typeof users>;
 
+const userSelectById = db
+    .select()
+    .from(users)
+    .where(eq(users.userId, sql.placeholder("id")))
+    .prepare("user_select_by_id");
+
+const userSelectByEmail = db
+    .select()
+    .from(users)
+    .where(eq(users.email, sql.placeholder("email")))
+    .limit(1)
+    .prepare("user_select_by_email");
+
 export const getUser = async (email: string) =>
-    await db.select().from(users).where(eq(users.email, email)).limit(1);
+    // await db.select().from(users).where(eq(users.email, email)).limit(1);
+    await userSelectByEmail.execute({ email: email });
 
 export const getUserById = async (id: string) =>
-    await db.select().from(users).where(eq(users.userId, id)).limit(1);
+    // await db.select().from(users).where(eq(users.userId, id)).limit(1);
+    await userSelectById.execute({ id: id });
 
 export const insertUser = async (user: NewUser) =>
     db.insert(users).values(user).returning();
+
+export const deleteUser = async (id: string) =>
+    await db.delete(users).where(eq(users.userId, id));
